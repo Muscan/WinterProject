c23dbc2a7cb01d1a9767538ed1621dd1b8c76b36
Mihai <kubikweg@gmail.com>
2021-12-22 08:37:08 +0200

    Added Session1 solution and Started exercices

---
+++ Session1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Session1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Winter");
+            //Create a console application that prints the current date and time.
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine(localDate);
+        }
+    }
+}
