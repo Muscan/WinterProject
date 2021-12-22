1791e46c54e7750ad1c0430080034861db8f2159
Mihai <kubikweg@gmail.com>
2021-12-22 08:30:42 +0200

    Add project files.

---
+++ WinterProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WinterProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
