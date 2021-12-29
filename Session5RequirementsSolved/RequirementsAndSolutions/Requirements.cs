6f7c25b598597c3705b97a1cdfc459406db47afb
Mihai <kubikweg@gmail.com>
2021-12-29 08:44:45 +0200

    Added requirements for the session

---
+++ Session5RequirementsSolved/RequirementsAndSolutions/Requirements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session5RequirementsSolved.RequirementsAndSolutions
+{
+    class Requirements
+    {/*
+        Exercise 1. 
+        Using any class implemented in the previous homeworks, split it into multiple files by using partial classes. 
+        
+        Exercise 2. 
+        Add an extension method to the class from Exercise 1. 
+        Add an extension method named SubstractTen to the int data type. Implement this method so that it substracts 10 from a number and returns the result. You should be able to call it like this: 7.SubstractTen() (the result is -1) or i.SubstractTen(), where i is a variable of type int and the result is i - 10. 
+        
+        Exercise 3. 
+        Create an enum which contains the months of a year. 
+        
+        Exercise 4. 
+        Create a desktop application. Add a button to the form, and implement the button's click event handler so that when the button is clicked, the application will display a message window. 
+        Add a textbox control to the window, and when clicking the button display the message from the textbox in the message box. 
+        Add a combobox (dropdown) control to the window and populate it either statically (from the designer) or dynamically (in the code, from a list, in the form's constructor, after the method InitializeComponent() is called)
+        Add labels for each of the controls in the window. 
+        Arrange all controls so that they are all aligned to each other and have the same size. 
+        Rename the controls so that instead of the default names they have meaningful names. 
+        
+        Exercise 5. 
+        Play with a desktop application by putting any controls you want on the form and try to see how they work. 
+        Suggestions: datetimepicker, checkbox/checkboxlist, openfiledialog, groupbox, picturebox, tooltip, etc. */
+    }
+}
