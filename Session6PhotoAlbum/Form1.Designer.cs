6ec783bb598b1f8e7f183c242863efea3584fe27
Mihai <kubikweg@gmail.com>
2021-12-29 12:36:15 +0200

    Added load imaged dynamic: -Load select and display images dynamic using file path selected by the user. - Added validation for the .png extension

---
+++ Session6PhotoAlbum/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace Session6PhotoAlbum
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.pctBox = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnLoad.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLoad.Location = new System.Drawing.Point(327, 356);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(128, 53);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // pctBox
+            // 
+            this.pctBox.Location = new System.Drawing.Point(183, 95);
+            this.pctBox.Name = "pctBox";
+            this.pctBox.Size = new System.Drawing.Size(416, 219);
+            this.pctBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctBox.TabIndex = 1;
+            this.pctBox.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pctBox);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Photo Album";
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.PictureBox pctBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Session6PhotoAlbum/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Session6PhotoAlbum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string filePath = openFileDialog1.FileName;
+            
+            if (Path.GetExtension(filePath) == ".png")
+            {
+                pctBox.Image = Image.FromFile(filePath);
+            }
+            else
+            {
+                MessageBox.Show(".png extensions must be selected");
+            }
+                
+        }
+    }
+}
