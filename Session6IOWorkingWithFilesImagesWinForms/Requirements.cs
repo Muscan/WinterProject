a6168c0e5c02846303ac3cf742527eff4e3ef3c7
Mihai <kubikweg@gmail.com>
2021-12-29 08:50:26 +0200

    Added IO project Skeleton

---
+++ Session6IOWorkingWithFilesImagesWinForms/Requirements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session6IOWorkingWithFilesImagesWinForms
+{
+    class Requirements
+    {
+        /*
+
+        Exercise 1. 
+        Implement a basic text editor, similar to Notepad. 
+        Required functionality:
+        - open a text file from the disk and display the text in the UI
+        - edit the text in the UI (Edit button)
+        - save the text back to file (Save button)
+        - enter text in teh editor and save it to a new file. (Dynaic path) folderBrowserDialog.SelectedPath;
+        
+        Exercise 2. 
+        Display a picture in a Windows Forms application. 
+        Try to implement this so that the user can choose what picture to display. (Drop down - img 1, 2, 3)
+        
+        Exercise 3. 
+        Define an application that you would like to implement. 
+        This does not include to implement the application, but only to define the functionality that it needs to support. 
+        The specifications need to be written down. 
+        If possible, use the text editor from exercise 1 for this.
+        
+        Using a public api, i want to see the weather using my gps position.
+        - Choose a public api
+        - Check the routes(calls)
+        - Implement a basic UI 
+         1. Using a search field to search location
+          a. Using a city name
+          b. Using a zip code
+          c. Using an UNICODE UTF 8(Enter english city name and display also other - Bors (border city) -> Borș
+         2. Using my location  SF :)
+         3. Display the city + degrees (in celsius) in format 21.5 C degrees
+        */
+    }
+}
