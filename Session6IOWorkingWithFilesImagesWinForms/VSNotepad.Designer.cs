79f9ade4df2eb2c9f169342ea2bedfdc97bd6536
Mihai <kubikweg@gmail.com>
2022-01-05 08:05:18 +0200

    Added helping comments for the flow; Added UX changes for CRUD

--- Session6IOWorkingWithFilesImagesWinForms/VSNotepad.Designer.cs
+++ Session6IOWorkingWithFilesImagesWinForms/VSNotepad.Designer.cs
@@ -83,15 +83,15 @@ namespace Session6IOWorkingWithFilesImagesWinForms
             // 
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
             this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(250, 32);
-            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(346, 32);
+            this.newToolStripMenuItem.Text = "New File txt";
             this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
             this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(250, 32);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(346, 32);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
@@ -99,14 +99,14 @@ namespace Session6IOWorkingWithFilesImagesWinForms
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(250, 32);
-            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(346, 32);
+            this.saveToolStripMenuItem.Text = "Edit And Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(250, 32);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(346, 32);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
--- Session6IOWorkingWithFilesImagesWinForms/VSNotepad.cs
+++ Session6IOWorkingWithFilesImagesWinForms/VSNotepad.cs
@@ -39,17 +39,23 @@ namespace Session6IOWorkingWithFilesImagesWinForms
         {
             //Open the FileExplorer Directory on the local PC-> Target
             //Take a folder in which we will save the notepad file
+            //In dr I hold the selected resulst(THE PATH -> NOT USED)
             DialogResult dr = folderBrowserDialog1.ShowDialog();
+
             //path to the file
             string folderPath = folderBrowserDialog1.SelectedPath;
             //name of the file
-            string fileName = txtBoxFileName.Text+".txt";
+            string fileName = txtBoxFileName.Text+".txt";//I would like add a drop down to selecte .pdf, .jpg etc. 
             string fullFileName = folderPath + "\\" + fileName;
+
             //Create file tests.txt
-            using (FileStream fs = File.Create(fullFileName))
+            //In File Class the Create Method is a static method -> that`s why It can be called without new
+            using FileStream fs = File.Create(fullFileName);
             {
                 //UTF8Enconding GetBytes() -> From a string returns a bytes array.
+                //Call the constructor UTF8Encoding
                 byte[] content = new UTF8Encoding().GetBytes(richTextBox1.Text);
+                //method Write from class FileStream is called by the fs Object
                 fs.Write(content, 0, content.Length);
                 //In Filestrean we write the content. Start from position 0 and write the lenght
 
@@ -63,15 +69,20 @@ namespace Session6IOWorkingWithFilesImagesWinForms
             //path to the file
             //string folderPath = folderBrowserDialog1.SelectedPath;
             //print the text from the file
+            //Method ShowDialog is called by the openFileDialog1
             openFileDialog1.ShowDialog();
+            //fullFileName -> global so that it can be used in other methods
             fullFileName = openFileDialog1.FileName;
             //https://stackoverflow.com/questions/25807209/validate-uploaded-file-extension
             if (Path.GetExtension(fullFileName) == ".txt")
             {
-                using (FileStream fs = File.OpenRead(fullFileName))
+                using FileStream fs = File.OpenRead(fullFileName);
                 {
+                    //fs.Length return the length of the file(text inside the file) and create byte array of the length(fs.Length)
                     byte[] content = new byte[fs.Length];
-                    UTF8Encoding encoder = new UTF8Encoding();
+                    //sugested fix from InteliSense
+                    UTF8Encoding encoder = new();
+                    //UTF8Encoding encoder = new UTF8Encoding();
                     fs.Read(content, 0, content.Length);
                     //transfrom from bytes to string
                     richTextBox1.Text = encoder.GetString(content);
