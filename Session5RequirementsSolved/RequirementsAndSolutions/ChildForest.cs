a624044e7b7ac7db6902c0b0b3289a3f8dbb5821
Mihai <kubikweg@gmail.com>
2022-01-05 10:22:52 +0200

    Played around with partial classes and Inheritance

---
+++ Session5RequirementsSolved/RequirementsAndSolutions/ChildForest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Session5RequirementsSolved.RequirementsAndSolutions
+{
+    partial class ChildForest: Forest
+    {
+         public void Call()
+        {
+            PrintAllInstanceHeight();
+        }
+    }
+
+    partial class ChildForest
+    {
+        public void noInheritance()
+        {
+            PrintAllInstanceHeight();
+        }
+    }
+}
