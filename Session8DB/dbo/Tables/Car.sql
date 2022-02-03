f966506a20471b0391de0ad2093b703873be36a2
Mihai <kubikweg@gmail.com>
2022-02-03 21:11:47 +0200

    Added DB Schema and DB project

---
+++ Session8DB/dbo/Tables/Car.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Car] (
+    [ID]         INT            IDENTITY (1, 1) NOT NULL,
+    [Name]       NCHAR (10)     NOT NULL,
+    [Engine]     DECIMAL (2, 1) NOT NULL,
+    [SeriesYear] INT            NOT NULL,
+    CONSTRAINT [PK_Cars] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ Session8DB/dbo/Tables/CarDPOes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CarDPOes] (
+    [ID]         INT             IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (MAX)  NULL,
+    [Engine]     DECIMAL (18, 2) NOT NULL,
+    [SeriesYear] INT             NOT NULL,
+    CONSTRAINT [PK_dbo.CarDPOes] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
