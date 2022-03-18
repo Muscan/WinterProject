b7c93cfa3b600930b913224df24232c8cfd19c4f
Mihai <kubikweg@gmail.com>
2022-03-18 07:52:38 +0200

    Replace nr. at postion in array Refactored code

--- ReversePrintReplaceNElement/Program.cs
+++ ReversePrintReplaceNElement/Program.cs
@@ -6,22 +6,23 @@ namespace ReversePrintReplaceNElement
     {
         static void Main(string[] args)
         {
-           
-            int[] ArrangedArray = { 1, 3, 6, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+            ReplaceNElement replaceNElement = new ReplaceNElement();
+            int[] ArrangedArray = { 1, 11, 3, 6, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 11, 19 };
+
             ReversePrint.ReverseArrayMethod(ArrangedArray);
             Console.WriteLine("\nReversed array is: ");
-            for (int i = 0; i < ArrangedArray.Length; i++)
-            {
-
-                Console.Write(" "+ArrangedArray[i]);
-            }
-
-            ReplaceNElement.ReplaceMethod(ArrangedArray, 1, 5000);
+            replaceNElement.IterateArray(ArrangedArray); 
+            
+            //Array.Reverse(ArrangedArray);
+            replaceNElement.ReplaceMethod(ArrangedArray, 1, 5000);
             Console.WriteLine("\nNew Array with replaced index is: ");
-            for (int i = 0; i < ArrangedArray.Length; i++)
-            {
-                Console.Write(" " + ArrangedArray[i]);
-            }
+            replaceNElement.IterateArray(ArrangedArray);
+
+
+            //replace a numbeer(11) with a new number(2)
+            Console.WriteLine("New array with number replaced ");
+            replaceNElement.ReplaceOccurence(ArrangedArray, 11, 790);
+            replaceNElement.IterateArray(ArrangedArray);
             
         }
     }
--- ReversePrintReplaceNElement/ReplaceNElement.cs
+++ ReversePrintReplaceNElement/ReplaceNElement.cs
@@ -8,7 +8,7 @@ namespace ReversePrintReplaceNElement
 {
     internal class ReplaceNElement
     {
-       public static void ReplaceMethod(int[] array, int indexToReplace, int replacedElemAtIndex)
+       public  void ReplaceMethod(int[] array, int indexToReplace, int replacedElemAtIndex)
         {
             if (indexToReplace < array.Length && indexToReplace >= 0)
             {
@@ -21,5 +21,36 @@ namespace ReversePrintReplaceNElement
                 Console.WriteLine("Out of this world error");
             }
         }
+
+        public  void ReplaceOccurence(int[] arr, int nrToBeReplaced, int newNumber)
+        {
+            int contor = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == nrToBeReplaced)
+                {
+                    arr[i] = newNumber;
+                    contor++;
+                }
+            }
+            if(contor > 0)
+            {
+                Console.WriteLine("Replaced " + contor + " numbers");
+            }
+            else
+            {
+                Console.WriteLine("None of the conditions were met.");
+            }
+        }
+        public  void IterateArray(int[]ArrangedArray)
+        {
+            for (int i = 0; i < ArrangedArray.Length; i++)
+            {
+                Console.Write(" " + ArrangedArray[i]);
+            }
+            Console.WriteLine();
+        }
+
+
     }
 }
